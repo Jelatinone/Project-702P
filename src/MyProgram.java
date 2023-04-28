623d5bb1690addc1ef75cb1931b98014407d4544
Jelatinone <115746975+Jelatinone@users.noreply.github.com>
2023-04-27 23:02:49 -0500

    Small Restructure

--- src/MyProgram.java
+++ src/MyProgram.java
@@ -1,8 +1,13 @@
 //Libraries
+
 import java.io.File;
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
@@ -25,19 +30,13 @@ public class MyProgram
          * <------------------------------->
          */
 
+        long InitTime = System.currentTimeMillis();
+        int HiccaCount = 0, WallieCount = 0, BeeperCount = 0, Beeper_TotalWordLength = 0;
+        double Hicca_AverageWorth, Hicca_TotalWorth = 0.0, Wallie_AverageSteps, Wallie_TotalSteps = 0.0, Beeper_AverageWordLength;
+        Scanner FileReader = new Scanner(new File("src\\lang701g.txt")); // <---- TODO: Change this value when switching between CodeHS and IDE
+        DecimalFormat Formatter = new DecimalFormat("###.##"); Formatter.setRoundingMode(RoundingMode.UP);
         ArrayList<Animal> Animals = new ArrayList<>();
         ArrayList<Character> Beeper_TotalCharacters = new ArrayList<>();
-        long InitTime = System.currentTimeMillis();
-        int HiccaCount = 0;
-        int WallieCount = 0;
-        int BeeperCount = 0;
-        int Beeper_TotalWordLength = 0;
-        double Hicca_TotalWorth = 0.0;
-        double Wallie_TotalSteps = 0.0;
-        double Hicca_AverageWorth;
-        double Wallie_AverageSteps;
-        double Beeper_AverageWordLength;
-        DecimalFormat Formatter = new DecimalFormat("###.##"); Formatter.setRoundingMode(RoundingMode.UP);
 
         //Working Common Letter Code; Outputs 'i'
 
@@ -59,16 +58,17 @@ public class MyProgram
         };
         */
 
-        //Testing Common Letter Code; Outputs' List Containing: 'i'
+        //Testing Common Letter Code; Output List Containing: 'a, e'
 
-        Callable<String> Beeper_CommonLetter = () -> {StringBuilder QuickAppend = new StringBuilder(); List<Character> Results = Beeper_TotalCharacters.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
-                .entrySet().stream().collect(Collectors.groupingBy(Map.Entry::getValue, Collectors.mapping(Map.Entry::getKey, Collectors.toList())))
-                .entrySet().stream().max(Map.Entry.comparingByKey()).map(Map.Entry::getValue).orElse(Collections.emptyList()); for(int i = 0; i < Results.size(); i++)
-                {QuickAppend.append(Results.get(i)).append((i == Results.size()-1)? (""): (","));} return QuickAppend.toString();};
+        Callable<String> Beeper_CommonLetter = () -> {StringBuilder ResultBuilder = new StringBuilder(); List<Character> Results = Beeper_TotalCharacters.stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting())).entrySet().stream().collect(Collectors.groupingBy(Map.Entry::getValue, Collectors
+                        .mapping(Map.Entry::getKey, Collectors.toList()))).entrySet().stream().max(Map.Entry.comparingByKey()).map(Map.Entry::getValue).orElse(Collections.emptyList());
+            for(int i = 0; i < Results.size(); i++){ResultBuilder.append(Results.get(i)).append((i == Results.size()-1)? (""): (", "));} return ResultBuilder.toString();};
 
         //Working File Estimation Lambda Code; Outputs # of Animal Objects
 
         Callable <Integer> ProjectedAnimalCount = () -> {
+
             Scanner PreFileReader = new Scanner(new File("src\\lang701g.txt"));
             int NumObjects = 0;
             while(PreFileReader.hasNextLine())
@@ -83,7 +83,7 @@ public class MyProgram
             }
             return NumObjects;
         };
-        Scanner FileReader = new Scanner(new File("src\\lang701g.txt"));
+
 
         /*
          * <------------------------------->
@@ -93,8 +93,9 @@ public class MyProgram
 
         while(FileReader.hasNextLine())
         {
-            TimeUnit.MILLISECONDS.sleep(100);
+            TimeUnit.MILLISECONDS.sleep(50);
             System.out.println("\033[H\033[2J");
+            System.out.println(Beepers.class.getConstructors()[1].getParameterCount());
             System.out.println(new StringBuilder("Parsing File: ").append(((Formatter.format(((double)(HiccaCount + WallieCount + BeeperCount) / ProjectedAnimalCount.call()) * 100)))).append("%\n"));
             switch(Integer.parseInt(FileReader.nextLine()))
             {
@@ -142,11 +143,11 @@ public class MyProgram
          * <------------------------------->
          */
 
-        System.out.println("Analysis Results\n----------------------\n");
-        System.out.println(new StringBuilder("The average value of the Hicca fur is: $").append(Formatter.format(Hicca_AverageWorth)));
-        System.out.println(new StringBuilder("The average number of steps taken by the Wallies is: ").append(Wallie_AverageSteps));
-        System.out.println(new StringBuilder("The average size of the Beepers words is: ").append(Beeper_AverageWordLength));
-        System.out.println(new StringBuilder("The most common letter(s) in all the Beepers' words is: ").append(Beeper_CommonLetter.call()));
-        System.out.println(new StringBuilder("\n\nProgram took ").append(System.currentTimeMillis() - InitTime).append("ms"));
+        System.out.println(new StringBuilder("Analysis Results\n----------------------\n")
+                .append("\nThe average value of the Hicca fur is: $").append(Formatter.format(Hicca_AverageWorth))
+                .append("\nThe average number of steps taken by the Wallies is: ").append(Wallie_AverageSteps)
+                .append("\nThe average size of the Beepers words is: ").append(Beeper_AverageWordLength)
+                .append("\nThe most common letter(s) in all the Beepers' words is: ").append(Beeper_CommonLetter.call())
+                .append("\n\nTime Elapsed: ").append(System.currentTimeMillis() - InitTime).append("ms"));
     }
 }
